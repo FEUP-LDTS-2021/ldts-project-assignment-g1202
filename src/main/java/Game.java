ccfc57efc771fb99f9b3f5735594e057b28be9d5
ZveiN <52889593+ZveiN@users.noreply.github.com>
2021-12-28 12:46:08 +0000

    Working menu + Survival/PVP modes selection
    2 Options working
    -With GUI
    -Without GUI (Not Sure how to elaborate here only using lanterna)

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,30 +2,114 @@ import com.googlecode.lanterna.SGR;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.gui2.*;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.Scanner;
 
 
+
 public class Game {
     //Iniciar variaveis - Terminais e screen
+
     Terminal terminal = new DefaultTerminalFactory().createTerminal();
     Screen screen = new TerminalScreen(terminal);
     TextGraphics tg = screen.newTextGraphics();
-    private Object Character;
+
+    JFrame window;
+    Container con;
+    JPanel titleNamePanel, startButtonPanel, choiceButtonPanel;
+    JLabel titleNameLabel;
+    Font titleFont = new Font("Times New Roman", Font.PLAIN, 70);
+    Font normalFont = new Font("Arial", Font.PLAIN, 30);
+    JButton startButton, surv, pvp;
+    TitleScreenHandler tsHandler = new TitleScreenHandler();
 
     public Game() throws IOException { // construtor de Game
         try {
+
+            //Terminal
+            window = new JFrame();
+            window.setSize(800,600);
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            window.getContentPane().setBackground(Color.black);
+            window.setLayout(null);
+            window.setVisible(true);
+            con = window.getContentPane();
+
+            //Painel que recebe titulo
+            titleNamePanel = new JPanel();
+            titleNamePanel.setBounds(100,70,600,150);
+            titleNamePanel.setBackground(Color.black);
+            titleNameLabel = new JLabel("DUNGEON BOY");
+            titleNameLabel.setForeground(Color.white);
+            titleNameLabel.setFont(titleFont);
+
+            //Painel que recebe o botão start
+            startButtonPanel = new JPanel();
+            startButtonPanel.setBounds(300,400,200,100);
+            startButtonPanel.setBackground(Color.black);
+
+            //Botao de start
+            startButton = new JButton("START");
+            startButton.setBackground(Color.black);
+            startButton.setForeground(Color.white);
+            startButton.setFont(normalFont);
+            startButton.addActionListener(tsHandler); // receber click do rato no botao start
+
+            //Adds para aparecerem no programa
+            titleNamePanel.add(titleNameLabel);
+            startButtonPanel.add(startButton);
+            con.add(titleNamePanel);
+            con.add(startButtonPanel);
+
             screen.startScreen(); // start
 
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public void createGameMenu(){
+
+        //Temos de "desligar" o botao start quando ele é carregado
+        startButtonPanel.setVisible(false);
+
+        choiceButtonPanel = new JPanel();
+        choiceButtonPanel.setBounds(120,275,500,100);
+        choiceButtonPanel.setBackground(Color.black);
+        con.add(choiceButtonPanel);
+
+        //Botão survival mode
+        surv = new JButton("Survival Mode");
+        surv.setBackground(Color.black);
+        surv.setForeground(Color.white);
+        surv.setFont(normalFont);
+        choiceButtonPanel.add(surv);
+
+        //Botão PVP
+        pvp = new JButton("PvP Mode");
+        pvp.setBackground(Color.black);
+        pvp.setForeground(Color.white);
+        pvp.setFont(normalFont);
+        choiceButtonPanel.add(pvp);
+    }
+    public class TitleScreenHandler implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            createGameMenu(); // cria nova janela com o menu
         }
     }
 
@@ -79,7 +163,7 @@ public class Game {
                         sb = new StringBuilder();
                         break;
                     case ArrowDown:
-                        System.out.println("baixo");
+                        //System.out.println("baixo");
                         break;
                     case ArrowLeft:
                         System.out.println("Esq");
