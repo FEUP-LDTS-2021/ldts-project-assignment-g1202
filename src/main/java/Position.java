4ea49f5bb61581455550d34ae807ea0b241cdef4
ZveiN <52889593+ZveiN@users.noreply.github.com>
2022-01-02 18:41:10 +0000

    Player Position file

---
+++ src/main/java/Position.java
@@ -0,0 +1,23 @@
+public class Position {
+    private int x;
+    private int y;
+
+    //construtor
+    public Position(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    // getters
+    public int getX(){  return x;}
+    public int getY(){  return y; }
+
+    //setters
+    public void setX(int x){
+        this.x = x;
+    }
+    public void setY(int y){
+        this.y = y;
+    }
+
+}
