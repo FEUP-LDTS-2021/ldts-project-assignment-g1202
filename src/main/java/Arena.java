b1d676c1aa70f1b0e95b90f8ea591cae16f7d40d
ZveiN <52889593+ZveiN@users.noreply.github.com>
2022-01-02 18:40:03 +0000

    Added Second Terminal screen
    Second terminal screen draw function for PVP mode

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -2,18 +2,36 @@ import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 
 public class Arena {
+    Player player;
     private int height;
     private int width;
 
-    public Arena(int width, int height){
+
+    public Arena(int width, int height) {
+        player = new Player(10, 10);
         this.height = height;
         this.width = width;
     }
 
-    public void draw(TextGraphics screen){
+    //Ecrã modo Survival
+    public void draw(TextGraphics screen) {
         screen.setBackgroundColor(TextColor.Factory.fromString("#906846"));
-        screen.fillRectangle(new TerminalPosition(0,0), new TerminalSize(width, height), ' ');
+        screen.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
+        player.draw(screen);
     }
+
+    // Ecrã para o modo PVP
+    public void draw2(TextGraphics screen2) {
+        screen2.setBackgroundColor(TextColor.Factory.fromString("#465690"));
+        screen2.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
+        player.draw(screen2);
+    }
+
+
 }
+
+
